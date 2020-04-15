ee71658fa309ec02cd131a05a006bc42238ba186
Daniel Gonzalez <d.gonzalez0902@gmail.com>
2020-04-15 17:54:21 +0200

    Properly apply gravity to jump
    - Previously, we were over-applying gravity

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -2,13 +2,13 @@ extends KinematicBody2D
 
 class_name Actor
 
-# export == [SerializeField]
-export var gravity := 4000.0
+# ! export == [SerializeField]
+export var gravity := 4000
 export var speed := Vector2(300.0, 1000.0)
 var _velocity := Vector2.ZERO
 
 
+# 	# TODO: move this to script later
 # func _physics_process(delta: float) -> void:
 # 	velocity.y += gravity * delta
-# 	# TODO: move this to script later
-# 	# velocity.y = max(speed.y, velocity.y)
+# 	# velocity.y = min(speed.y, velocity.y)
--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -3,15 +3,15 @@ extends Actor
 # TODO: implement double jump
 
 
-# * when overriding parent's function, subclasses will implicitly call super(func)
 func _physics_process(delta: float) -> void:
+	var direction := get_direction()
 	var is_jump_interrupted := (
 		Input.is_action_just_released("jump")
 		and _velocity.y < 0.0
 	)
-	var direction := get_direction()
+
 	_velocity = calculate_move_velocity(
-		_velocity, direction, speed, delta, is_jump_interrupted
+		_velocity, direction, delta, is_jump_interrupted
 	)
 	move_and_slide(_velocity, Vector2.UP)
 
@@ -30,19 +30,18 @@ func get_direction() -> Vector2:
 func calculate_move_velocity(
 	linear_velocity: Vector2,
 	direction: Vector2,
-	speed: Vector2,
 	delta: float,
 	is_jump_interrupted: bool
 ) -> Vector2:
 	var new_velocity := linear_velocity
-	new_velocity.x = speed.x * direction.x
-	# TODO: test adding this back in when gravity is working
-	# velocity.y = min(speed.y, velocity.y)
-	_velocity.y += gravity * delta
-	# TODO: determine why player only jumps for one frame
-	# if direction.y == -1.0: # ! this logic is wrong
-	if direction.y != 0.0:
+	new_velocity.x = direction.x * speed.x
+	new_velocity.y += gravity * delta
+
+	# check for jumps
+	if direction.y == -1.0:
 		new_velocity.y = speed.y * direction.y
+
 	if is_jump_interrupted:
 		new_velocity.y = 0.0
+
 	return new_velocity
