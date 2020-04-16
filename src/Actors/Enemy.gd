ddce695467439fef5b67b5dda5b79658c4f08eed
Daniel Gonzalez <d.gonzalez0902@gmail.com>
2020-04-16 22:33:03 +0200

    Add enemy-player collision

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -9,6 +9,15 @@ func _ready() -> void:
 	_velocity.x = -speed.x
 
 
+func _on_StompDetector_area_entered(area: Area2D) -> void:
+	print("entered stomper")
+	# if player is below the enemy's stomper position, don't die
+	if area.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	# destroy enemy
+	queue_free()
+
+
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
 	if is_on_wall():
