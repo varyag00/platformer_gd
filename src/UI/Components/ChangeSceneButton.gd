2d311f06ba0131fd03177e865cec78f1a381de47
Daniel Gonzalez <d.gonzalez0902@gmail.com>
2020-04-23 18:46:43 +0200

    Bring pause menu ui to top layer

--- src/UI/Components/ChangeSceneButton.gd
+++ src/UI/Components/ChangeSceneButton.gd
@@ -5,7 +5,15 @@ export(String, FILE) var next_scene_path := ""
 
 
 func _on_button_up() -> void:
+	PlayerData.reset_game()
 	get_tree().change_scene(next_scene_path)
+
+	# # ! possibly include this?
+	# PlayerData.score = 0
+	# # TODO: test setting scene tree var
+	# get_tree().paused = false
+	# get_tree().reload_current_scene()
+
 
 
 func _get_configuration_warning() -> String:
